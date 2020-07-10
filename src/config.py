21ffb9be1dc3f39a3b4fe29c80854125953893ff
Ashish D'Souza <sudouser512@gmail.com>
2020-07-09 22:39:26 -0400

    Update

---
+++ src/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# config.py
+
+IP = '0.0.0.0'
+PORT = 80
+
+USERNAME = ''  # Root user username
+PASSWORD = ''  # Root user password
---
+++ src/server/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# auth.py
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from config import
+
+
+def authenticate():
+    pass
