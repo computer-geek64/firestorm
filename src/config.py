0391e299f37acaf69c8ebd05e954c032e3e2abf8
Ashish D'Souza <sudouser512@gmail.com>
2020-07-11 01:45:30 -0400

    Update

--- src/config.py
+++ src/config.py
@@ -12,3 +12,7 @@ PASSWORD_HASH = ''  # Root user SHA-512 password hash
 
 VIDEOS_LOCATION = ''  # Path to videos folder
 MUSIC_LOCATION = ''  # Path to music folder
+
+PROJECTS_DATABASE = 'projects'  # PostgreSQL projects database name
+PROJECTS_DB_USER = ''  # PostgreSQL projects database user
+PROJECTS_DB_PASSWORD = ''  # PostgreSQL projects database user password
