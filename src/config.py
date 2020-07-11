abd02954d503f2baa6a5b18858d974fa199406aa
Ashish D'Souza <sudouser512@gmail.com>
2020-07-11 13:48:21 -0400

    Added initial postgresql code

--- src/config.py
+++ src/config.py
@@ -13,6 +13,6 @@ PASSWORD_HASH = ''  # Root user SHA-512 password hash
 VIDEOS_LOCATION = ''  # Path to videos folder
 MUSIC_LOCATION = ''  # Path to music folder
 
-PROJECTS_DB = 'projects'  # PostgreSQL projects database name
-PROJECTS_DB_USER = ''  # PostgreSQL projects database user
-PROJECTS_DB_PASSWORD = ''  # PostgreSQL projects database user password
+DB_USER = ''  # PostgreSQL database user
+DB_PASSWORD = ''  # PostgreSQL database user password
+PROJECTS_DB_NAME = 'projects'  # PostgreSQL projects database name
--- src/server/projects_blueprint.py
+++ src/server/projects_blueprint.py
@@ -2,9 +2,10 @@
 # projects_blueprint.py
 
 import os
+import psycopg2
 from auth import authenticate
 from errors_blueprint import *
-from config import PROJECTS_DATABASE
+from config import PROJECTS_DB_NAME, PROJECTS_DB_USER, PROJECTS_DB_PASSWORD
 from flask import Blueprint, render_template, request, session
 
 
@@ -15,4 +16,10 @@ projects_blueprint = Blueprint('projects_blueprint', __name__, template_folder=o
 @projects_blueprint.route('/projects/', methods=['GET'])
 @authenticate
 def get_projects():
+    conn = psycopg2.connect(database=PROJECTS_DB_NAME, user=PROJECTS_DB_USER, password=PROJECTS_DB_PASSWORD, host='localhost', port=5432)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM projects;')
+    cursor.fetchall()
+    conn.commit()
+    conn.close()
     return render_template('projects/projects.html')
