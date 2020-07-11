bcb70eb19c7e6ac4fda133c63285ed3023e6fe35
Ashish D'Souza <sudouser512@gmail.com>
2020-07-11 15:08:13 -0400

    Add SQL files

---
+++ src/db/create.sql
@@ -0,0 +1,36 @@
+-- create.sql
+-- Drop tables if already exists
+DROP TABLE IF EXISTS "organization_type" CASCADE;
+DROP TABLE IF EXISTS "organization" CASCADE;
+DROP TABLE IF EXISTS "language" CASCADE;
+DROP TABLE IF EXISTS "project" CASCADE;
+
+
+-- Create tables
+-- Create table organization type
+CREATE TABLE "organization_type" (
+    "label" VARCHAR (32) PRIMARY KEY
+);
+
+-- Create table organization
+CREATE TABLE "organization" (
+    "name" VARCHAR (64) PRIMARY KEY,
+    "type" VARCHAR (32) NOT NULL REFERENCES "organization_type" ("label")
+)
+
+-- Create table language
+CREATE TABLE "language" (
+    "name" VARCHAR (32),
+    "extension" VARCHAR (8),
+    "color" VARCHAR (8)
+);
+
+-- Create table project
+CREATE TABLE "project" (
+    "name" VARCHAR (64) PRIMARY KEY,
+    "description" VARCHAR (512) NOT NULL,
+    "organization" VARCHAR (64) NOT NULL REFERENCES "organization" ("name"),
+    "language" VARCHAR (32) REFERENCES "language" ("name"),
+    "starred" BOOLEAN NOT NULL DEFAULT FALSE,
+    "created" TIMESTAMP NOT NULL DEFAULT current_timestamp;
+);
---
+++ src/db/populate.sql
@@ -0,0 +1,89 @@
+-- populate.sql
+-- Insert into table organization_type
+INSERT INTO "organization_type" (
+                "label"
+            )
+     VALUES (
+                'personal'
+            ),
+            (
+                'school'
+            ),
+            (
+                'work'
+            ),
+            (
+                'hackathon'
+            );
+
+-- Insert into table organization
+INSERT INTO "organization" (
+                "name",
+                "type"
+            )
+     VALUES (
+                'Personal',
+                'personal'
+            ),
+            (
+                'Georgia Institute of Technology',
+                'school'
+            ),
+            (
+                'Nead Werx, Inc.',
+                'work'
+            ),
+            (
+                'HackGT',
+                'hackathon'
+            ),
+            (
+                'SwampHacks',
+                'hackathon'
+            ),
+            (
+                'Optical Science Center for Applied Research (OSCAR)',
+                'work'
+            );
+
+-- Insert into table language
+INSERT INTO "language" (
+                "name",
+                "extension",
+                "color"
+            )
+     VALUES (
+                'Python',
+                'py',
+                '#3572a5'
+            ),
+            (
+                'Java',
+                'java',
+                '#b07219'
+            ),
+            (
+                'Ruby',
+                'rb',
+                '#701516'
+            ),
+            (
+                'Shell',
+                'sh',
+                '#89e051'
+            ),
+            (
+                'HTML',
+                'html',
+                '#e34c26'
+            ),
+            (
+                'CSS',
+                'css',
+                '#563d7c'
+            ),
+            (
+                'JavaScript',
+                'js',
+                '#f1e05a'
+            );
