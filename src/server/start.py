5c2f06ee4289300a6273bb20fed122de04b75001
Ashish D'Souza <sudouser512@gmail.com>
2020-12-29 17:37:46 -0500

    Update start.py

--- src/server/start.py
+++ src/server/start.py
@@ -5,10 +5,10 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import requests
-from config import USB_KEY_UUID, UNPLUG_USB_KEY_WEBHOOK
+from config import USB_KEY_UUID, IFTTT_WEBHOOK
 from subprocess import Popen, PIPE
 
 
 if os.path.exists(os.path.join('/dev/disk/by-uuid', USB_KEY_UUID)):
     Popen(['/usr/bin/umount', os.path.join('/dev/disk/by-uuid', USB_KEY_UUID)], stdout=PIPE, stderr=PIPE).communicate()
-    requests.post(UNPLUG_USB_KEY_WEBHOOK)
+    requests.post(IFTTT_WEBHOOK, json={'value1': 'Unplug USB Key', 'value2': 'Firestorm has unlocked all drives successfully.'})
