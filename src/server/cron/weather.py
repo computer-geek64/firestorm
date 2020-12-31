bffa48488be3c28a485a5216a7ed0e65a6322dd8
Ashish D'Souza <sudouser512@gmail.com>
2020-12-30 21:52:55 -0500

    Fixed Python 3 hashbang

--- src/server/cron/weather.py
+++ src/server/cron/weather.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3 -B
+# weather.py
+
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
--- src/server/on_boot.py
+++ src/server/on_boot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -B
 # on_boot.py
 
 import os
